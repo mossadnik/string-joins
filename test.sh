dbe39717a559ff711f3e9fd77d7d1278eb1d4b6b
Christopher Prohm <mail@cprohm.de>
2021-06-22 22:05:49 +0200

    Add helper script to run tests

---
+++ test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# cargo test
+cargo build --release
+
+if [ ! -e generalized_suffix_array.so ]
+then
+    ln -s target/release/libgeneralized_suffix_array.so generalized_suffix_array.so
+fi
+
+python -m pytest tests/
